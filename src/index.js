08bd7b874c16e1b593454724e5f069fba71e31d2
Leonardo Benítez <benitez.leonardo.py@gmail.com>
2021-07-12 11:13:40 -0400

    :white_check_mark: Add support for multi-lang

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import { getQuote } from './quotes'
 
+const PATH_LANG = {
+  '/es': 'es',
+  '/en': 'en',
+}
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-  const quote = await getQuote()
-  return new Response(quote, {
-    headers: { 'content-type': 'application/json' },
-  })
+  const { pathname } = new URL(request.url)
+  const language = PATH_LANG[pathname]
+
+  if (language) {
+    const quote = await getQuote(language)
+    return new Response(quote, {
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+  } else {
+    return new Response('The language path is not valid', {
+      headers: {
+        status: 400,
+        statusText: 'Bad Request',
+        'content-type': 'text/plain',
+      },
+    })
+  }
 }
--- src/quotes.js
+++ src/quotes.js
@@ -1,11 +1,16 @@
 import { getRandomIndex, getJson } from './utils'
 
-const QUOTES_TABLE_EN = 'Quotes'
-const QUOTES_TABLE_ES = 'Frases'
+const DEFAULT_QUOTES_TABLE = 'Quotes'
 const QUOTES_API_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/`
 
-const getQuotes = async () => {
-  const quotesUrl = `${QUOTES_API_URL}${QUOTES_TABLE_EN}`
+const QUOTES_LANG_TABLE = {
+  es: 'Frases',
+  en: 'Quotes',
+}
+
+const getQuotes = async lang => {
+  const quotesUrl = `${QUOTES_API_URL}${QUOTES_LANG_TABLE[lang] ||
+    DEFAULT_QUOTES_TABLE}`
   const quotesData = await getJson(quotesUrl, {
     headers: {
       authorization: `Bearer ${AIRTABLE_API_KEY}`,
@@ -15,8 +20,8 @@ const getQuotes = async () => {
   return quotesData.records.map(r => r.fields)
 }
 
-const getQuote = async () => {
-  const quotes = await getQuotes()
+const getQuote = async lang => {
+  const quotes = await getQuotes(lang)
   return JSON.stringify(quotes[getRandomIndex(quotes.length)])
 }
 
